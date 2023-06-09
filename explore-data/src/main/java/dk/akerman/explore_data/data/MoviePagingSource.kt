9183eb309b48f24a6b63fc83cd08f94b2a226484
Asger <43748689+AsgerAkerman@users.noreply.github.com>
2023-06-09 10:18:37 +0200

    Pagingsource changes

--- explore-data/src/main/java/dk/akerman/explore_data/data/MoviePagingSource.kt
+++ explore-data/src/main/java/dk/akerman/explore_data/data/MoviePagingSource.kt
@@ -2,36 +2,26 @@ package dk.akerman.explore_data.data
 
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
-import dk.akerman.explore_data.data.model.MovieRemote
+import dk.akerman.explore_data.domain.Movie
 
 class MoviePagingSource(
     private val movieRemoteDataSource: MovieRemoteDataSource
-) : PagingSource<Int, MovieRemote>() {
-    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, MovieRemote> {
-        val page = params.key ?: 1
-        return try {
-            val response = movieRemoteDataSource.fetchMovies(page)
-
-            if (response.isSuccessful) {
-                val movies = response.body()?.results
-
-                val prevKey = if (page > 1) page - 1 else null
-                val nextKey = if (page < (response.body()?.totalPages ?: 0)) page + 1 else null
-
-                if (movies != null) {
-                    LoadResult.Page(data = movies, prevKey = prevKey, nextKey = nextKey)
-                } else {
-                    LoadResult.Error(Exception("No movies found"))
-                }
-            } else {
-                LoadResult.Error(Exception("Failed to fetch movies: ${response.code()}"))
-            }
-        } catch (e: Exception) {
-            LoadResult.Error(e)
-        }
+) : PagingSource<Int, Movie>() {
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Movie> {
+        val nextPage = params.key ?: 1
+        return movieRemoteDataSource.fetchMovies(nextPage).fold(
+            onSuccess = {
+                LoadResult.Page(
+                    data = it.mapToDomain().results,
+                    prevKey = null,
+                    nextKey = it.page
+                )
+            },
+            onFailure = { LoadResult.Error(it) }
+        )
     }
 
-    override fun getRefreshKey(state: PagingState<Int, MovieRemote>): Int? {
+    override fun getRefreshKey(state: PagingState<Int, Movie>): Int? {
         // The refresh key is the key for the first page (page 1) or null if it doesn't exist
         return state.anchorPosition?.let { anchorPosition ->
             state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
